3ec24bb7412a5240eec414bf73919094a4d9db42
pc-v2 <meguminxkazuma@protonmail.com>
2021-12-12 22:30:01 +0700

    adding redraw

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const random = (min = 1, max = 12) => {
+const random = (min = 1, max = 14) => {
   let num = Math.random() * (max - min) + min;
   return Math.round(num);
 };
+
 // initialize HasBlackJack
 let HasBlackJack = false;
 let isAlive = true;
+let firstCard = random();
+let secondCard = random();
+let cards = [firstCard, secondCard];
+let sum = cards[0] + cards[1];
+let newCard = 0;
 
 function startGame() {
   renderGame();
@@ -14,13 +20,8 @@ function renderGame() {
   let want_el = document.getElementById("want-el");
   let card_El = document.getElementById("cards-el");
   let sum_El = document.getElementById("sum-el");
-
-  let firstCard = random();
-  let secondCard = random();
   let button_el = document.getElementById("buttonRand");
-  let sum = firstCard + secondCard;
-
-  card_El.textContent = "Cards: " + firstCard + ", " + secondCard;
+  card_El.textContent = "Cards: " + cards[0] + ", " + cards[1];
   sum_El.textContent = "Sum: " + sum;
 
   if (sum < 21) {
@@ -38,17 +39,9 @@ function renderGame() {
 
 let button_redo_el = document.getElementById("buttonRedo");
 function redrawCard() {
+  newCard = random();
+  cards.push(newCard);
+  sum += newCard;
+  console.log(cards);
   renderGame();
 }
-
-// Draw Cash !!!
-
-//practice boolean conditions
-
-// console.log(4 === 3); //false
-// console.log(5 > 2); //true
-// console.log(12 > 12); //false
-// console.log(3 < 0); //false
-// console.log(3 >= 3); //true
-// console.log(11 <= 11); //true
-// console.log(3 <= 2); //false
