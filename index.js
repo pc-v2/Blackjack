d472f0f7bf45b766ba3e6fa2cc24917ecc4d4243
pc-v2 <meguminxkazuma@protonmail.com>
2021-12-14 12:45:06 +0700

    update game

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
-const random = (min = 1, max = 14) => {
-  let num = Math.random() * (max - min) + min;
-  return Math.round(num);
+let player = {
+  name: "Andrew",
+  chips: 100,
 };
 
-// initialize HasBlackJack
-let HasBlackJack = false;
+function getRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 13);
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+// initialize hasBlackJack
+let hasBlackJack = false;
 let isAlive = true;
-let firstCard = random();
-let secondCard = random();
+let firstCard = getRandomNumber();
+let secondCard = getRandomNumber();
 let cards = [firstCard, secondCard];
 let sum = cards[0] + cards[1];
 let newCard = 0;
+let messageEl = document.getElementById("message-el");
+let cardEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
+let buttonEl = document.getElementById("buttonRand");
+let buttonRedoEl = document.getElementById("buttonRedo");
+let playerEl = document.getElementById("player-el");
+
+buttonRedoEl.style.visibility = "hidden";
+
+playerEl.textContent = player.name + " " + player.chips;
 
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  let want_el = document.getElementById("want-el");
-  let card_El = document.getElementById("cards-el");
-  let sum_El = document.getElementById("sum-el");
-  let button_el = document.getElementById("buttonRand");
-  card_El.textContent = "Cards: " + cards[0] + ", " + cards[1];
-  sum_El.textContent = "Sum: " + sum;
+  buttonRedoEl.style.visibility = "visible";
+  cardEl.textContent = "Cards: ";
+
+  for (let i = 0; i < cards.length; i++) {
+    cardEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
 
   if (sum < 21) {
-    want_el.textContent = "Do you want to draw a new card? 😊";
+    messageEl.textContent = "Do you want to draw a new card? 😊";
   } else if (sum === 21) {
-    want_el.textContent = "You've Got a Blackjack 😎";
-    HasBlackJack = true;
+    messageEl.textContent = "You've Got a Blackjack 😎";
+    hasBlackJack = true;
   } else {
-    want_el.textContent = "sad lol 😢";
+    messageEl.textContent = "sad lol 😢";
+    isAlive = false;
   }
-  button_el.style.visibility = "hidden";
-  console.log(sum_El.textContent);
+  buttonEl.style.visibility = "hidden";
+  console.log(sumEl.textContent);
   // console.log(isAlive);
 }
 
-let button_redo_el = document.getElementById("buttonRedo");
 function redrawCard() {
-  newCard = random();
-  cards.push(newCard);
-  sum += newCard;
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    newCard = getRandomNumber();
+    cards.push(newCard);
+    sum += newCard;
+    console.log(cards);
+    renderGame();
+  }
 }
